ea0596e2f812f4b2ffdfe0bcb3802157d34f1f6d
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-10 03:07:10 -0800

    structure set up for dashboard, sensors, add sensor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import { Button } from 'antd';
 import './App.css';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Button</Button>
+      <Sidebar/>
     </div>
   );
 }
