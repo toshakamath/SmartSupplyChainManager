2b44f2ce0dc34971723e45f6ad90e6ccbf11bbd8
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-20 20:17:48 -0800

    get user details

--- backend/Controller/User.js
+++ backend/Controller/User.js
@@ -1,4 +1,4 @@
-const tableName = "smartsupplychain_db.users"
+const tableName = "smartsupplychain_db.users";
 
 let login = (request, response) => {
   console.log("request.BODY: ", request.body);
@@ -17,28 +17,45 @@ let login = (request, response) => {
   });
 };
 //update for get customers with role only supplier
-let getAllCustomers = (request, response) =>{
-    console.log("request.BODY: ", request.body);
-    let sql = `SELECT * FROM ${tableName}`;
-    request.sqldb.query(sql, (err, result) => {
-      if (err) {
-        console.log(err);
+let getAllCustomers = (request, response) => {
+  let sql = `SELECT * FROM ${tableName}`;
+  request.sqldb.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.length == 0) {
+        response.json({ status: "error", reason: "no users found" });
       } else {
-        if (result.length == 0) {
-          response.json({ status: "error", reason: "no users found" });
-        } else {
-          console.log(result[0]);
-          result.forEach(user => {
-            delete user.password;
-          });
-          response.json({ status: "success", reason: "list of users", users: result});
-        }
+        console.log(result[0]);
+        result.forEach((user) => {
+          delete user.password;
+        });
+        response.json({
+          status: "success",
+          reason: "list of users",
+          users: result,
+        });
       }
-    });
-}
+    }
+  });
+};
 
-let getCustomerDetails = (request, response) =>{
-    return response.json({"status":"success"})
-}
+let getCustomerDetails = (request, response) => {
+  console.log("request.PARAMS: ", request.query);
+  let sql = `SELECT * FROM ${tableName} WHERE email='${request.query.email}'`;
+  request.sqldb.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.length == 0) {
+        response.json({ status: "error", reason: "user not found" });
+      } else {
+        console.log(result[0]);
+        delete result[0].password;
+        response.json({ status: "success", reason: "user information", user_details:result[0] });
+      }
+    }
+  });
+};
 
-module.exports={login, getAllCustomers, getCustomerDetails}
+module.exports = { login, getAllCustomers, getCustomerDetails };
