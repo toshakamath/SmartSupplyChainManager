d2c5df2f2293e6bac370a2daa28d8fc4cc0d651f
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-20 22:25:29 -0800

    add a sensor in a warehouse api

--- backend/Controller/Sensor.js
+++ backend/Controller/Sensor.js
@@ -1,26 +1,52 @@
 
-const getSensorDetails = (req, res) => {
-    return res.json({"status":"success"})
+const getSensorDetails = (request, response) => {
+    // const collection = request.mongodb.collection("sensors");
+    // collection.find({id: request.params.id}).toArray(function (err, docs) {
+    //     if(err){
+    //         console.log(err);
+    //         response.json({ status: "error", reason: err });
+    //     }
+    //     else{
+    //         console.log("Found the following records");
+    //         console.log(docs);
+    //         if(docs.length === 0){
+    //             response.json({ status: "error", reason: "no sensors in this warehouse" });
+    //         }
+    //         else{
+    //             response.json({ status: "success", reason: "sensor details", sensors: docs});
+    //         }
+    //     }
+    // });
 }
 
-const getSensorTypeAndUnit = (req, res) => {
-    return res.json({"status":"success"})
+const getSensorTypeAndUnit = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const getSensorHistory = (req, res) => {
-    return res.json({"status":"success"})
+const getSensorHistory = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const addSensorInWarehouse = (req, res) => {
-    return res.json({"status":"success"})
+//request should contain warehouse_id also check whether warehouse ID exists before inserting 
+const addSensorInWarehouse = (request, response) => {
+    const collection = request.mongodb.collection('sensor');
+    collection.insertOne(request.body, function(err, docs) {
+        if(err){
+            console.log(err)
+            response.json({ status: "error", reason: err });
+        }
+        else{
+            response.json({ status: "success", reason: "sensor details"});
+        }
+      });
 }
 
-const deleteSensor = (req, res) => {
-    return res.json({"status":"success"})
+const deleteSensor = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const updateSensor = (req, res) => {
-    return res.json({"status":"success"})
+const updateSensor = (request, response) => {
+    return response.json({"status":"success"})
 }
 
 module.exports={getSensorDetails, getSensorTypeAndUnit, getSensorHistory, addSensorInWarehouse, deleteSensor, updateSensor}
