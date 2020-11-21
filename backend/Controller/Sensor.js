ee52be0bd8e858f32a3f0e879850503ee4da6858
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-20 19:57:02 -0800

    sensor apis skeleton

---
+++ backend/Controller/Sensor.js
@@ -0,0 +1,26 @@
+
+const getSensorDetails = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const getSensorTypeAndUnit = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const getSensorHistory = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const addSensorInWarehouse = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const deleteSensor = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const updateSensor = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+module.exports={getSensorDetails, getSensorTypeAndUnit, getSensorHistory, addSensorInWarehouse, deleteSensor, updateSensor}
