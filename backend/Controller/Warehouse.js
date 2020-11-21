8fb503a4fbe79642a6a09e5cef6f2a9a56ee3024
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-20 20:28:37 -0800

    get all warehouse details api

--- backend/Controller/Warehouse.js
+++ backend/Controller/Warehouse.js
@@ -1,26 +1,40 @@
-
-const getAllWarehouses = (req, res) => {
-    return res.json({"status":"success"})
+const getAllWarehouses = (request, response) => {
+    const collection = request.mongodb.collection("warehouse");
+    collection.find({}).toArray(function (err, docs) {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("Found the following records");
+            console.log(docs);
+            if(docs.length === 0){
+                response.json({ status: "error", reason: "no results found" });
+            }
+            else{
+                response.json({ status: "success", reason: "list of warehouses", warehouses: docs});
+            }
+        }
+    });
 }
 
-const getWarehousesForCustomer = (req, res) => {
-    return res.json({"status":"success"})
+const getWarehousesForCustomer = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const getAllSensorsForWarehouse = (req, res) => {
-    return res.json({"status":"success"})
+const getAllSensorsForWarehouse = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const addWarehouse = (req, res) => {
-    return res.json({"status":"success"})
+const addWarehouse = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const deleteWarehouse = (req, res) => {
-    return res.json({"status":"success"})
+const deleteWarehouse = (request, response) => {
+    return response.json({"status":"success"})
 }
 
-const updateWarehouse = (req, res) => {
-    return res.json({"status":"success"})
+const updateWarehouse = (request, response) => {
+    return response.json({"status":"success"})
 }
 
 module.exports={getAllWarehouses, getWarehousesForCustomer, getAllSensorsForWarehouse, addWarehouse, deleteWarehouse, updateWarehouse}
