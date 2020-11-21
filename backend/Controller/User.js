7ce38c36ff4ab526d776f37ef33eb264d6768537
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-20 20:05:54 -0800

    get all customers api

--- backend/Controller/User.js
+++ backend/Controller/User.js
@@ -16,19 +16,22 @@ let login = (request, response) => {
     }
   });
 };
-
+//update for get customers with role only supplier
 let getAllCustomers = (request, response) =>{
     console.log("request.BODY: ", request.body);
-    let sql = `SELECT * FROM ${tableName} WHERE email='${request.body.email}' AND password='${request.body.password}'`;
+    let sql = `SELECT * FROM ${tableName}`;
     request.sqldb.query(sql, (err, result) => {
       if (err) {
         console.log(err);
       } else {
         if (result.length == 0) {
-          response.json({ status: "error", reason: "user not found" });
+          response.json({ status: "error", reason: "no users found" });
         } else {
           console.log(result[0]);
-          response.json({ status: "success", reason: "logged in successfully" });
+          result.forEach(user => {
+            delete user.password;
+          });
+          response.json({ status: "success", reason: "list of users", users: result});
         }
       }
     });
