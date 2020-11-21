70a5a572cda7cfb9d8c6e04af2f8233d4a3bd4f3
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-20 19:57:28 -0800

    warehouse apis skeleton

--- backend/Controller/Warehouse.js
+++ backend/Controller/Warehouse.js
@@ -3,4 +3,24 @@ const getAllWarehouses = (req, res) => {
     return res.json({"status":"success"})
 }
 
-module.exports={getAllWarehouses}
+const getWarehousesForCustomer = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const getAllSensorsForWarehouse = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const addWarehouse = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const deleteWarehouse = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+const updateWarehouse = (req, res) => {
+    return res.json({"status":"success"})
+}
+
+module.exports={getAllWarehouses, getWarehousesForCustomer, getAllSensorsForWarehouse, addWarehouse, deleteWarehouse, updateWarehouse}
